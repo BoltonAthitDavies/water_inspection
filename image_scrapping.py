25939632a65dfcfd411a5090db2e4595dd2c0290
BoltonAthitDavies <72595767+BoltonAthitDavies@users.noreply.github.com>
2024-06-13 22:16:33 +0700

    update image_scrapping.py 13/6/24

--- image_scrapping.py
+++ image_scrapping.py
@@ -3,8 +3,11 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import time
+import requests
+from PIL import Image
+from io import BytesIO
 
 class RPA:
     def __init__(self, url):
@@ -22,16 +25,39 @@ class RPA:
         hope = []
         script_contents = str(script_contents)
 
-        split = str(script_contents).split('<')
+        #split = str(script_contents).split('"')
+        split = str(script_contents).split('app-images')
         for i in split:
             if filter in i:
                 hope.append(i)
         return hope
+    
+    def savePic(self, url):
+        try:
+            # Send a GET request to the URL
+            response = requests.get(url)
+            response.raise_for_status()  # Check for HTTP errors
+
+            # Open the image
+            image = Image.open(BytesIO(response.content))
+
+            # # Display the image (optional)
+            # image.show()
+
+            # Save the image to a file
+            hope = url[83:134]
+            image.save(f'./images/' + hope + '.jpg')
+
+            print('Image downloaded and saved successfully!')
+
+        except requests.exceptions.HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except Exception as err:
+            print(f'Other error occurred: {err}')
 
 
 if __name__ == '__main__':
     login_url = 'https://pm-rsm.cpretailink.co.th/login'
-    plan_search_url = 'https://pm-rsm.cpretailink.co.th/pm-plan-search'
 
     try:
         loginPage = RPA(login_url)
@@ -53,37 +79,123 @@ if __name__ == '__main__':
         ## กดปุ่ม enter เพื่อ login
         password.send_keys(Keys.RETURN)
 
-        time.sleep(1)
+        time.sleep(1.5)
 
         ## เช็ค element ของปุ่ม "ทั้งหมด"
         all_Button = loginPage.driver.find_element(By.ID, 'mat-button-toggle-7-button') 
         ## กดปุ่ม "ทั้งหมด"
         all_Button.click()
         
-        time.sleep(1)
+        time.sleep(1.5)
 
         ## element ของปุ่ม "โปรดเลือกบริษัท"
         cop_Button = driver.find_element(By.CSS_SELECTOR, 'input.form-control[placeholder="โปรดเลือก"]')
         ## กดปุ่ม "โปรดเลือกบริษัท"
         cop_Button.click()
 
-        time.sleep(1)
+        time.sleep(1.5)
 
         ## element ของ multi selected "เลือกบริษัท"
         cop_Button = driver.find_element(By.CSS_SELECTOR, 'angular2-multiselect.ng-untouched.ng-pristine.ng-valid')
         ## กด multi selected "เลือกบริษัท"
         cop_Button.click()
         
-        time.sleep(1)
+        time.sleep(1.5)
+
+        ## หา element ของ "Seven Eleven"
+        seven11_element = driver.find_element(By.XPATH, '//li[label[text()="Seven Eleven"]]/label')
+        ## กดปุ่ม "Seven Eleven"
+        seven11_element.click()
+
+        time.sleep(1.5)
+
+        ## element ของปุ่ม "เสร็จสิ้น"
+        finish_Button = driver.find_element(By.XPATH, '//button[contains(text(), "เสร็จสิ้น")]')
+        ## กดปุ่ม "เสร็จสิ้น"
+        finish_Button.click()
+
+        time.sleep(1.5)
+
+        ## element ของปุ่ม "ประเภทสัญญา"
+        contract_Button = driver.find_element(By.XPATH, '/html/body/app-root/app-plan-search/div/div/div[2]/app-search-pm-box/div/form/div[4]/div[2]/app-multi-search-box/div/input')
+        ## กดปุ่ม "ประเภทสัญญา"
+        contract_Button.click()
+
+        time.sleep(1.5)
+
+        ## element ของ multi selected "เลือกสัญญา"
+        selectcont_Button = driver.find_element(By.CSS_SELECTOR, 'angular2-multiselect.ng-untouched.ng-pristine.ng-valid')
+        ## กด multi selected "เลือกสัญญา"
+        selectcont_Button.click()
+
+        time.sleep(1.5)
+        
+        ## หา element ของ "สัญญาล้างถัง PP 3 เดือน"
+        PPtank_element = driver.find_element(By.XPATH, '//li[label[text()="สัญญาล้างถัง PP 3 เดือน"]]/label')
+        ## กดปุ่ม "สัญญาล้างถัง PP 3 เดือน"
+        PPtank_element.click()
+
+        time.sleep(1.5)
+
+        ## element ของปุ่ม "เสร็จสิ้น"
+        finish_Button = driver.find_element(By.XPATH, '//button[contains(text(), "เสร็จสิ้น")]')
+        ## กดปุ่ม "เสร็จสิ้น"
+        finish_Button.click()
+
+        time.sleep(1.5)
+
+        ## element ของปุ่ม "ค้นหา"
+        search_Button = driver.find_element(By.XPATH, '//button[contains(text(), "ค้นหา")]')
+        ## กดปุ่ม "ค้นหา"
+        search_Button.click()
+
+        time.sleep(2)
+        # path หน้าตารางรวมแผนงาน
+        table_path = "/html/body/app-root/app-e-service-plan/div/full-calendar/div[2]/div/table/tbody/tr/td/div/div/div/table/tbody/"
+        
+        ##   row   day             button                                                                                                             
+        ## /tr[2]/td[7]/div/div[2]/div[1]/a
+
+        ## หาวันแรกของเดือนนั้นๆ
+        row = 1
+        day = 1
+        button = 2
+        for i in range(1,8):
+            try:
+                monthPlan_Button = driver.find_element(By.XPATH, table_path + f'tr[{row}]/td[{i}]/div/div[2]/div[{button}]/a')
+            except:
+                day += 1
+                continue
+       
+        ## element ของปุ่ม "n แผน" ในวันที่ 1 มิ.ย. 2024
+        monthPlan_Button = driver.find_element(By.XPATH, table_path + f'tr[1]/td[{day}]/div/div[2]/div[{button}]/a')
+        ## กดปุ่ม "n แผน" ในวันที่ 1 มิ.ย. 2024
+        monthPlan_Button.click()
+
+        time.sleep(2)
+
+        # row  column
+        # tr[1]/td[4]
+        # path หน้าตารางแผนงาน ณ เดือนที่เลือก
+        sub_table_path = '/html/body/app-root/app-e-service-table/div/app-table-contract/div/table/tbody/'
+         ## element ของปุ่ม รูปภาพ ในวันที่ 1 มิ.ย. 2024
+        pic_button = driver.find_element(By.XPATH, sub_table_path + 'tr[1]/td[4]')
+        ## กดปุ่ม รูปภาพ ในวันที่ 1 มิ.ย. 2024
+        pic_button.click()
+
+        time.sleep(2)
 
         ## print html script ของหน้านี้
-        search_script_contents = loginPage.getpageScript()
-        search_list = loginPage.getList(search_script_contents, 'li')
-        print(search_list)
+        pic_link_contents = loginPage.getpageScript()
+        pic_list = loginPage.getList(pic_link_contents, '"')
+        print(pic_list[1])
 
-        selected_element = driver.find_element(By.CSS_SELECTOR, 'li.pure-checkbox.ng-star-inserted')
-        seven11_element = selected_element.find_element(By.CSS_SELECTOR, 'Seven Eleven')
-    
+        # for url in pic_list:
+        #     url = url.replace("&amp;", "&")
+        #     # print(url)
+        #     loginPage.savePic(url)
+
+
     except Exception as e:
         print(f"An error occurred: {e}")
 
--- selenium_tutorial.py
+++ selenium_tutorial.py
@@ -1,23 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import requests
+from PIL import Image
+from io import BytesIO
 
-driver = webdriver.Chrome()
+# Direct URL to the S3 image
+#url = 'https://rssm-public-bucket-prd.s3.ap-southeast-1.amazonaws.com/pm-checklist-images/2024-06-01_9ba30a36-ff7e-48f5-8520-24a969fba2fd.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIA237GGB7L4XU7UJ62%2F20240613%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Date=20240613T124457Z&X-Amz-Expires=1800&X-Amz-Signature=ef4201de042b1c8866436a98ff3dfa7ce26863f019e2b3790e117ab2c623acbf&X-Amz-SignedHeaders=host&x-id=GetObject'
+hope = "https://rssm-public-bucket-prd.s3.ap-southeast-1.amazonaws.com/pm-checklist-images/2024-06-01_9ba30a36-ff7e-48f5-8520-24a969fba2fd.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&amp;X-Amz-Credential=AKIA237GGB7L4XU7UJ62%2F20240613%2Fap-southeast-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20240613T125454Z&amp;X-Amz-Expires=1800&amp;X-Amz-Signature=5219c4dec08b663794433582d94bd8253f157e2670de7e2fff3a7f771914908c&amp;X-Amz-SignedHeaders=host&amp;x-id=GetObject"
+url = hope.replace("&amp;", "&")
+try:
+    # Send a GET request to the URL
+    response = requests.get(url)
+    response.raise_for_status()  # Check for HTTP errors
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    # Open the image
+    image = Image.open(BytesIO(response.content))
 
-title = driver.title
+    # # Display the image (optional)
+    # image.show()
 
-driver.implicitly_wait(0.5)
+    # Save the image to a file
+    image.save('./images/downloaded_image.jpg')
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    print('Image downloaded and saved successfully!')
+    
+    print()
 
-text_box.send_keys("Selenium")
-submit_button.click()
-
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
-
-print(text)
-
-driver.quit()
+except requests.exceptions.HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
